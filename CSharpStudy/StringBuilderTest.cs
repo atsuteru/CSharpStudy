d46f998fa9cbf7c4820802ebf39c07d3b48d0041
KAMIYA.Atsuteru <a-kamiya@pscsrv.co.jp>
2020-08-18 09:55:19 +0900

    StringBuilderTest: ループ計測を追加

--- CSharpStudy/StringBuilderTest.cs
+++ CSharpStudy/StringBuilderTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
 
 namespace CSharpStudy
 {
@@ -11,20 +12,55 @@ namespace CSharpStudy
         public void Concat1()
         {
             int numberOfItem = 4;
+            int lineCounter = 0;
+
+            var watch = new Stopwatch();
 
             Debug.WriteLine("5ŒÂ");
             //wata
             Debug.WriteLine("--- wata ---");
-            Debug.WriteLine(numberOfItem+"ŒÂ");
+            watch.Start();
+            while(watch.ElapsedMilliseconds <= 3000)
+            {
+                Debug.WriteLine(numberOfItem + "ŒÂ");
+                lineCounter++;
+            }
+            watch.Stop();
+            Debug.WriteLine($"---End of wata--- score:{lineCounter:#,##0}");
+            Thread.Sleep(3000);
+
             //tachi
             Debug.WriteLine("--- tachi ---");
+            lineCounter = 0;
+            watch.Reset();
+            watch.Start();
             var builder = new StringBuilder();
-            builder.Append(numberOfItem);
-            builder.Append("ŒÂ");
-            Debug.WriteLine(builder.ToString());
+            while (watch.ElapsedMilliseconds <= 3000)
+            {
+                builder.Clear();
+                builder.Append(numberOfItem);
+                builder.Append("ŒÂ");
+                Debug.WriteLine(builder.ToString());
+                lineCounter++;
+            }
+            watch.Stop();
+            Debug.WriteLine($"---End of tachi--- score:{lineCounter:#,##0}");
+            Thread.Sleep(3000);
             //kami
             Debug.WriteLine("--- kami ---");
-            Debug.WriteLine($"{numberOfItem}ŒÂ");
+            lineCounter = 0;
+            watch.Reset();
+            watch.Start();
+            while (watch.ElapsedMilliseconds <= 3000)
+            {
+                Debug.WriteLine($"{numberOfItem}ŒÂ");
+                lineCounter++;
+            }
+            watch.Stop();
+            Debug.WriteLine($"---End of kami--- score:{lineCounter:#,##0}");
+            Thread.Sleep(3000);
+
+
         }
     }
 }
